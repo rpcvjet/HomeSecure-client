5773c5a3134cf15ae3275b22eb954dba60558566
Kenneth Ashley Edwards <kenneth.ashley@gmail.com>
2017-03-08 17:24:41 -0800

    button trial

--- index.js
+++ index.js
@@ -5,52 +5,26 @@
 // const raspi = require('raspi');
 // const gpio = require('raspi-gpio');
 
-
-// raspi.init(() => {
-//   const input = new gpio.DigitalInput('P1-3');
-//   const output = new gpio.DigitalOutput('P1-5');
-//   output.write(input.read());
-// });
-
-// 
-// var raspi = require('raspi-io');
-// var five = require('johnny-five');
-// var board = new five.Board({
-//   io: new raspi()
-// });
 var Gpio = require('onoff').Gpio,
-  led = new Gpio(17, 'out');
-
-
-var iv = setInterval(function(){
-  led.writeSync(led.readSync() === 0 ? 1 : 0);
-}, 500);
-
-// Stop blinking the LED and turn it off after 5 seconds.
-setTimeout(function() {
+  led = new Gpio(16, 'out');
+  var button = new Gpio(17, 'in', 'both');
   console.log('Is this working?');
-  clearInterval(iv); // Stop blinking
-  led.writeSync(0);  // Turn LED off.
-  led.unexport();    // Unexport GPIO and free resources
-  console.log('Yes it is...');
-}, 5000);
 
-// board.on('ready', function() {
-
+    button.watch(function(err, value) {
+    if (err) exit();
+    led.writeSync(led.readSync() === 0 ? 1 : 0);
+    console.log('BUTTTTTTTON');
+  });
 
 
+  function exit() {
+    buzzer.unexport();
+    button.unexport();
+    process.exit();
+  }
+  process.on('SIGINT', exit);
 
 
-
-
-//
-// var Gpio = require('onoff').Gpio;
-//   led = new Gpio(14, 'out'),
-//   button = new Gpio(4, 'in', 'both');
-//
-// button.watch(function(err, value) {
-//   led.writeSync(value);
-// });
 
 
 console.log('If this came up without the yes then nope.');
