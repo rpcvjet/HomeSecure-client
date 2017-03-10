03e6167eb652cfb15dcd7f0d24180eef490911ee
Kenneth Ashley Edwards <kenneth.ashley@gmail.com>
2017-03-10 08:51:59 -0800

    kick off program with button click attempt

--- index.js
+++ index.js
@@ -9,20 +9,33 @@ module.exports = exports = {};
 exports.tempPassword;
 
 var Gpio = require('onoff').Gpio,
-led = new Gpio(17, 'out');
+  led = new Gpio(17, 'out');
+button = new Gpio(19, 'in', 'both');
+
+button.watch(function(err, value) {
+  if (err) exit();
+  buzzer.writeSync(value);
 
 
+  led.writeSync(0)
+  new Sound(`${__dirname}/assets/welcome.wav`).play();
+  takePicture()
+  .then(() => passwordReader())
+  .then(password => {
+    console.log('you said', password)
+    return  unlockDoor(password)
+  })
+  .then(() => {
+    console.log('access granted')
+    led.writeSync(1)
+    new Sound(`${__dirname}/assets/recognized.wav`).play();
+  })
 
-led.writeSync(0)
-new Sound(`${__dirname}/assets/welcome.wav`).play();
-takePicture()
-.then(() => passwordReader())
-.then(password => {
-	console.log('you said', password)
-	return  unlockDoor(password)
-})
-.then(() => {
-	console.log('access granted')
-	led.writeSync(1)
-	new Sound(`${__dirname}/assets/recognized.wav`).play();
-})
+});
+
+function exit() {
+  button.unexport();
+  process.exit();
+}
+
+process.on('SIGINT', exit);
