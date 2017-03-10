2ebe2d75df642f93dc1aa6d54a0e951a3c49e139
irvinemd55 <irvinedowning@gmail.com>
2017-03-09 18:53:19 -0800

    sounds added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const takePicture = require('./controllers/photo');
 const passwordReader = require('./controllers/password');
 const unlockDoor = require('./controllers/unlock');
-
+const Sound = require('node-aplay');
 
 module.exports = exports = {};
 exports.tempPassword;
@@ -12,7 +12,7 @@ var Gpio = require('onoff').Gpio,
 led = new Gpio(17, 'out');
 
 led.writeSync(0)
-
+new Sound(`${__dirname}/assets/welcome.wav`).play();
 takePicture()
 .then(() => passwordReader())
 .then(password => {
@@ -23,5 +23,3 @@ takePicture()
 	console.log('access granted')
 	led.writeSync(1)
 })
-
-
